6d5e60b24ac96619c002a6af003df4ff09578b55
ilanGrynszpan <ilan.grynszpan@gmail.com>
2019-11-10 23:54:15 -0300

    user data being received correctly on services page 3

--- ionic_front/src/app/tab3/tab3.page.ts
+++ ionic_front/src/app/tab3/tab3.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { Storage } from '@ionic/storage';
 
 @Component({
   selector: 'app-tab3',
@@ -8,7 +9,41 @@ import { Router } from '@angular/router';
 })
 export class Tab3Page {
 
-  constructor(private router: Router) {}
+  private stored_data:any = {
+
+    user:'',
+    services:[
+      {
+        servico: {
+          area_atuacao: "",
+          nome_servico: ""
+        },
+        
+        carteira: {
+          
+        }
+      }
+    ]
+  };
+
+  current_service = 0;
+  number_of_services = 0;
+
+  constructor(private router: Router,
+    private storage: Storage) {}
+  
+  ngOnInit() {
+
+    this.storage.get('user_info').then((val_user) => {
+
+      this.storage.get('services_info').then((val_services) => {
+
+        this.stored_data.services = val_services;
+        this.stored_data.user = val_user.nome;
+        this.number_of_services = val_services.length;
+      })
+    });
+  }
 
   goToEditarServicos(){
     this.router.navigate(['/editar-servicos'])
